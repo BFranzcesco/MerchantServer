ed884d7544716a8353408d724efcb1340f94f74c
BFranzcesco <franzb475@gmail.com>
2016-04-30 17:43:14 +0200

    defines register as post

--- src/main/java/com/fbtm/OrderController.java
+++ src/main/java/com/fbtm/OrderController.java
@@ -1,14 +1,16 @@
 package com.fbtm;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
     private String registrationID;
 
-    @RequestMapping("/register")
-    public void register(String registrationID) {
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public void register(@RequestBody String registrationID) {
         this.registrationID = registrationID;
     }
 
